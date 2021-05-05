a1553491738cda3eddda91e52644a60ad5858a0d
Krasowska Anna <annakrasowska2@gmail.com>
2021-05-05 14:58:53 +0200

    Base project w/ CommandHandler and BaseViewModel

---
+++ ProgramManager/lib/BaseViewModel.cs
@@ -0,0 +1,11 @@
+﻿using PropertyChanged;
+using System.ComponentModel;
+
+namespace LogDownloader
+{
+    [AddINotifyPropertyChangedInterface]
+    public class BaseViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged = (sender, e) => { };
+    }
+}
