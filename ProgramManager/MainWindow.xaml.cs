38ef789fbbee0cb1bb3832de1f33f43246fc03fa
Krasowska Anna <annakrasowska2@gmail.com>
2021-05-06 08:41:15 +0200

    InstalledSoftware singleton with GetInstalledSoftwareDictionary (Name, Version pair)

--- ProgramManager/MainWindow.xaml.cs
+++ ProgramManager/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ProgramManager.SystemUtility;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +24,11 @@ namespace ProgramManager
         public MainWindow()
         {
             InitializeComponent();
+            InstalledSoftware l = InstalledSoftware.GetInstance();
+            foreach(KeyValuePair<string, string> entry in l.InstalledSoftwareList)
+            {
+                System.Diagnostics.Trace.WriteLine("Name => "+entry.Key+" with version => "+entry.Value);
+            }
         }
     }
 }
---
+++ ProgramManager/SystemUtility/InstalledSoftware.cs
@@ -0,0 +1,102 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProgramManager.SystemUtility
+{
+    class InstalledSoftware
+    {
+        private static InstalledSoftware _instance;
+        public static InstalledSoftware GetInstance()
+        {
+            if (_instance == null)
+            {
+                _instance = new InstalledSoftware();
+            }
+            return _instance;
+        }
+
+        public Dictionary<string, string> InstalledSoftwareList {
+            get => GetInstalledSoftwareDictionary();
+        }
+
+        private InstalledSoftware() { }
+
+        private Dictionary<string, string> GetInstalledSoftwareDictionary()
+        {
+            string displayName;
+            string displayVersion;
+            Dictionary<string, string> installedSoftwareDictionary = new Dictionary<string, string>();
+
+            using (RegistryKey key = Registry.CurrentUser.OpenSubKey(@"SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall", false))
+            {
+                foreach (String keyName in key.GetSubKeyNames())
+                {
+                    RegistryKey subkey = key.OpenSubKey(keyName);
+                    displayName = subkey.GetValue("DisplayName") as string;
+                    displayVersion = subkey.GetValue("DisplayVersion") as string;
+                    displayVersion = string.IsNullOrEmpty(displayVersion) ? "unknown" : displayVersion;
+
+                    if (string.IsNullOrEmpty(displayName) || installedSoftwareDictionary.ContainsKey(displayName.ToLower()))
+                        continue;
+
+                    installedSoftwareDictionary.Add(displayName.ToLower(), displayVersion.ToLower());
+                }
+            }
+
+            using (var localMachine = RegistryKey.OpenBaseKey(RegistryHive.LocalMachine, RegistryView.Registry64))
+            {
+                var key = localMachine.OpenSubKey(@"SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall", false);
+                foreach (String keyName in key.GetSubKeyNames())
+                {
+                    RegistryKey subkey = key.OpenSubKey(keyName);
+                    displayName = subkey.GetValue("DisplayName") as string;
+                    displayVersion = subkey.GetValue("DisplayVersion") as string;
+                    displayVersion = string.IsNullOrEmpty(displayVersion) ? "unknown" : displayVersion;
+
+                    if (string.IsNullOrEmpty(displayName) || installedSoftwareDictionary.ContainsKey(displayName.ToLower()))
+                        continue;
+
+                    installedSoftwareDictionary.Add(displayName.ToLower(), displayVersion.ToLower());
+                }
+            }
+
+            using (var localMachine = RegistryKey.OpenBaseKey(RegistryHive.LocalMachine, RegistryView.Registry32))
+            {
+                var key = localMachine.OpenSubKey(@"SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall", false);
+                foreach (String keyName in key.GetSubKeyNames())
+                {
+                    RegistryKey subkey = key.OpenSubKey(keyName);
+                    displayName = subkey.GetValue("DisplayName") as string;
+                    displayVersion = subkey.GetValue("DisplayVersion") as string;
+                    displayVersion = string.IsNullOrEmpty(displayVersion) ? "unknown" : displayVersion;
+
+                    if (string.IsNullOrEmpty(displayName) || installedSoftwareDictionary.ContainsKey(displayName.ToLower()))
+                        continue;
+
+                    installedSoftwareDictionary.Add(displayName.ToLower(), displayVersion.ToLower());
+                }
+            }
+
+            using (RegistryKey key = Registry.LocalMachine.OpenSubKey(@"SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall", false))
+            {
+                foreach (String keyName in key.GetSubKeyNames())
+                {
+                    RegistryKey subkey = key.OpenSubKey(keyName);
+                    displayName = subkey.GetValue("DisplayName") as string;
+                    displayVersion = subkey.GetValue("DisplayVersion") as string;
+                    displayVersion = string.IsNullOrEmpty(displayVersion) ? "unknown" : displayVersion;
+
+
+                    if (string.IsNullOrEmpty(displayName) || installedSoftwareDictionary.ContainsKey(displayName.ToLower()))
+                        continue;
+
+                    installedSoftwareDictionary.Add(displayName.ToLower(), displayVersion.ToLower());
+                }
+            }
+            return installedSoftwareDictionary;
+        }
+
+    }
+}
