91aa5a77eb331ea5a788be10b1251a7a97151caf
Krasowska Anna <annakrasowska2@gmail.com>
2021-05-06 12:48:07 +0200

    software model

---
+++ ProgramManager/SystemUtility/Software/InstalledSoftwareHandler.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProgramManager.SystemUtility
+{
+    class InstalledSoftwareHandler
+    {
+    }
+}
---
+++ ProgramManager/SystemUtility/Software/Software.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProgramManager.SystemUtility
+{
+    class Software
+    {
+        string Name { get; set; }
+        string Version { get; set; }
+        bool isUpdateNeeded { get; set; }
+        public Software(string name, string version)
+        {
+            this.Name = name;
+            this.Version = version;
+            isUpdateNeeded = false;
+        }
+    }
+}
